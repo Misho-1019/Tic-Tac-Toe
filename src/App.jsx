436d7b27ac7cd0bdfe721198e49c2bff9d5a712b
Misho1019 <mikailtorres99@gmail.com>
2025-02-28 06:48:45 +0200

    Created checkWinner function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [gameState, setGameState] = useState(Array(9).fill(null))
+  const [currentPlayer, setCurrentPlayer] = useState('X')
+
+  const checkWinner = (newGameState) => {
+    const winningCombos = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ]
+
+    for (const combo of winningCombos) {
+      const [a, b, c] = combo;
+      if (newGameState[a] && newGameState[a] === newGameState[b] && newGameState[a] === newGameState[c]) {
+        return newGameState[a]
+      }
+    }
+    return null;
+  }
+
 
   return (
     <>
