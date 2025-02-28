b864d1c2e73b6bbe75fa5751e2c435af7cfc97c7
Misho1019 <mikailtorres99@gmail.com>
2025-02-28 06:39:40 +0200

    Add my jsx code

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,14 @@ function App() {
 
   return (
     <>
-      
+      <div className='container'>
+        <h1>Sea Chess (Tic-Tac-Toe)</h1>
+        <div className='board'>
+
+        </div>
+        <h2>Next Player: </h2>
+        <button className='reset'>Reset Game</button>
+      </div>
     </>
   )
 }
